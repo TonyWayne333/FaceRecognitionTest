40825bed4bf76cbdb50a47a72b45039459133690
Neel Shah <neel1040@gmail.com>
2019-11-06 20:46:08 -0500

    One more unit test added

--- TestFaceRecognition.py
+++ TestFaceRecognition.py
@@ -4,10 +4,15 @@ import faceRecognition
 
 
 class MyTestCase(unittest.TestCase):
-    def test_removefiles(self):
-        removefile = faceRecognition.remove_files
-        removefile(r'C:\Users\Public\testingPython')
-        self.assertEqual(len(os.listdir(r'C:\Users\Public\testingPython')), 0)
+    def test_download_files(self):
+        download_file = faceRecognition.download_files
+        download_file(r'C:\Users\Public\testingPython1\\', r'C:\Users\Public\testingPython2\test.jpg')
+        self.assertGreater(len(os.listdir(r'C:\Users\Public\testingPython2')), 0)
+
+    def test_remove_files(self):
+        remove_file = faceRecognition.remove_files
+        remove_file(r'C:\Users\Public\testingPython1')
+        self.assertEqual(len(os.listdir(r'C:\Users\Public\testingPython1')), 0)
 
 
 if __name__ == '__main__':
